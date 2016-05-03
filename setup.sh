63d9b89232acaab90335ce2a5d34025c07329133
wangzz <wzzvictory_tjsd@163.com>
2016-05-03 10:43:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/Use
 # rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
 SRC_HOME=`pwd`
+
+# 创建软链结(symbolic link) 
 ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+
 echo ${SRC_HOME}
 echo '***********************Done****************************'
